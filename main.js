9db4a55160620aa13d8a1d4ab3f4313e095ad42a
frixels <67122965+frixels@users.noreply.github.com>
2024-12-08 02:34:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
+document.addEventListener('DOMContentLoaded', () => {
+    const homePage = document.getElementById('home-page');
+    const container = document.querySelector('.container');
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    })
-    tabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-    tab.classList.add('active')
-    target.classList.add('active')
-  })
-})
+    function createSnowflake() {
+        const snowflakesContainer = document.getElementById('snowflakes');
+        const snowflake = document.createElement('div');
+        snowflake.classList.add('snowflake');
+
+        const size = Math.random() * 5 + 2;
+        snowflake.style.width = `${size}px`;
+        snowflake.style.height = `${size}px`;
+
+        const left = Math.random() * window.innerWidth;
+        snowflake.style.left = `${left}px`;
+
+        const animationDuration = Math.random() * 5 + 5;
+        snowflake.style.animationDuration = `${animationDuration}s`;
+
+        snowflakesContainer.appendChild(snowflake);
+    }
+
+    let snowflakeInterval = setInterval(() => {
+        createSnowflake();
+    }, 50);
+
+    setTimeout(() => {
+        clearInterval(snowflakeInterval);
+    }, 10000);
+
+    homePage.style.display = 'none';
+    container.style.display = 'flex';
+});
